066e083703f95b213aeea470ed38276e0bbbf737
Jeff Chang <jeffrey.chang2014@gmail.com>
2015-07-16 21:44:52 -0700

    Fix association for Type model

--- waitless/app/models/type.rb
+++ waitless/app/models/type.rb
@@ -1,4 +1,4 @@
 class Type < ActiveRecord::Base
 	has_many :restaurant_types
-	has_many :types, through: :restaurant_types
+	has_many :restaurants, through: :restaurant_types
 end
--- waitless/db/migrate/20150716225350_create_users.rb
+++ waitless/db/migrate/20150716225350_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-    	t.string :name
-    	t.string :email
-    	t.string :password_hash
-    	t.string :phone_number
+    	t.string   :name
+    	t.string   :email
+    	t.string   :password_hash
+    	t.string   :phone_number
 
       t.timestamps null: false
     end
