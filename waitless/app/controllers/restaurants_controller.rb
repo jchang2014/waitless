87616021f3b1f43707ceff550c96d5274fc7ed44
Jeff Chang <jeffrey.chang2014@gmail.com>
2015-07-18 10:32:28 -0700

    Include geolocation data for each search result

--- waitless/app/controllers/restaurants_controller.rb
+++ waitless/app/controllers/restaurants_controller.rb
@@ -1,9 +1,21 @@
 class RestaurantsController < ApplicationController
 	def index
-		# parameters = { term: params[:search], limit:10}
-		# params[:location].length > 0 ? @location_filter = params[:location] : @location_filter = "San Francisco"
+		parameters = { term: params[:search], limit:10}
+		params[:location].length > 0 ? @location_filter = params[:location] : @location_filter = "San Francisco"
 
-  # 	results = Yelp.client.search(@location_filter, parameters).as_json
+  	results = Yelp.client.search(@location_filter, parameters).as_json
+  	array = []
+  	for i in 0..9
+  		@business = results['hash']['businesses'][i]
+  		array.push(
+  		[
+	  	@name = @business['name'],
+	  	@location = @business['location']['address'][0],
+	  	@image = @business['image_url']
+	  	])
+	  	@lat_long = @business['location']['coordinate']
+	  end
+
 
   # 	array = []
   # 	for i in 0..9
