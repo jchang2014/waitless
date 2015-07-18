6ae73b9fccd4321012c216de30224924df13db9a
Patrick S. Betti <patreek3@gmail.com>
2015-07-17 17:42:17 -0700

    change users table to include uid, ref tok, acc tok, expr

--- waitless/db/migrate/20150716225350_create_users.rb
+++ waitless/db/migrate/20150716225350_create_users.rb
@@ -5,6 +5,11 @@ class CreateUsers < ActiveRecord::Migration
     	t.string   :email
     	t.string   :password_hash
     	t.string   :phone_number
+      t.string   :uid
+      t.string   :refresh_token
+      t.string   :access_token
+      t.string   :expires
+
 
       t.timestamps null: false
     end
