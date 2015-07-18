6442177d89e00de56be3b3d57b9ad5e58d2ed045
Jennifer Jaochico <jenjao515@gmail.com>
2015-07-18 09:50:17 -0700

    layout organized and colors in

--- waitless/config/application.rb
+++ waitless/config/application.rb
@@ -12,7 +12,7 @@ require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 #require yelp
-require "yelp"
+# require "yelp"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
