87d386315464c054ee3025ef247edb1c45fa82fe
Diana Lozano & Jeff Chang <pair+cutofmyjib.jchang2014@devbootcamp.com>
2015-07-18 17:36:58 -0700

    Add first 20 seeds, base loc SOMA

--- waitless/db/seeds.rb
+++ waitless/db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+results = [
+  "mourad-restaurant-san-francisco",
+  "the-flying-falafel-san-francisco-3",
+  "marlowe-san-francisco-2",
+  "cockscomb-san-francisco",
+  "garaje-san-francisco",
+  "zero-zero-san-francisco",
+  "box-kitchen-san-francisco",
+  "so-san-francisco-4",
+  "tropisueño-san-francisco-3",
+  "the-pink-elephant-san-francisco-3",
+  "citizens-band-san-francisco",
+  "little-skillet-san-francisco-2",
+  "jamber-san-francisco",
+  "picnic-on-third-san-francisco",
+  "hrd-san-francisco-2",
+  "dragoneats-san-francisco-3",
+  "freshroll-vietnamese-rolls-and-bowls-san-francisco",
+  "red-dog-restaurant-and-bar-san-francisco",
+  "bluestem-brasserie-san-francisco",
+  "archive-bar-and-kitchen-san-francisco"
+
+]
+
+results.map {
+  |result| Restaurant.create(yelp_id: result)
+}
