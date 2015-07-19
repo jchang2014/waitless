511be0639ff59aecc5b508d06d671bc5f03d9225
Diana Lozano & Jeff Chang <pair+cutofmyjib.jchang2014@devbootcamp.com>
2015-07-18 17:55:46 -0700

    Add 60 seeds for restaurant

--- waitless/app/controllers/restaurants_controller.rb
+++ waitless/app/controllers/restaurants_controller.rb
@@ -1,35 +1,31 @@
 class RestaurantsController < ApplicationController
 	def index
 		parameters = { term: "restaurants", limit:20}
-		@location_filter = "SOMA"
+		@location_filter = "Embarcadero"
     iterator = 0
   	response = Yelp.client.search(@location_filter, parameters).as_json
-  	# @results = []
-  	# for i in 0..9
-  	# 	@business = response['hash']['businesses'][i]
-  	# 	@categories = []
-  	# 	@business['categories'].each do |category|
-  	# 		@categories.push(category[0])
-  	# 	end
+  	@results = []
+  	for i in 0..9
+  		@business = response['hash']['businesses'][i]
+  		@categories = []
+  		@business['categories'].each do |category|
+  			@categories.push(category[0])
+  		end
 
-  	# 	@results.push(
-  	# 	{
-	  # 	name: "#{@business['name']}",
-	  # 	location: @business['location']['display_address'],
-	  # 	image: "#{@business['image_url']}",
-	  # 	rating: @business['rating_img_url'],
-	  # 	categories: @categories.join(', '),
-	  # 	latitude: @business['location']['coordinate']['latitude'],
-	  # 	longitude: @business['location']['coordinate']['longitude']
-	  # 	})
-  	# end
+  		@results.push(
+  		{
+	  	name: "#{@business['name']}",
+	  	location: @business['location']['display_address'],
+	  	image: "#{@business['image_url']}",
+	  	rating: @business['rating_img_url'],
+	  	categories: @categories.join(', '),
+	  	latitude: @business['location']['coordinate']['latitude'],
+	  	longitude: @business['location']['coordinate']['longitude']
+	  	})
+  	end
 
-  	# render 'index', locals: {results: @results}
-    p "*" * 50
-    while iterator < 20
-      p response['hash']['businesses'][iterator]['id']
-      iterator += 1
-    end
+  	render 'index', locals: {results: @results}
+
 	end
 
   def show
--- waitless/db/seeds.rb
+++ waitless/db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-results = [
+results_soma = [
   "mourad-restaurant-san-francisco",
   "the-flying-falafel-san-francisco-3",
   "marlowe-san-francisco-2",
@@ -27,9 +27,64 @@ results = [
   "red-dog-restaurant-and-bar-san-francisco",
   "bluestem-brasserie-san-francisco",
   "archive-bar-and-kitchen-san-francisco"
-
 ]
 
-results.map {
+results_mission = [
+"sous-beurre-kitchen-san-francisco-2",
+"thistle-san-francisco-8",
+"bistro-l-aviateur-san-francisco-2",
+"hoffmanns-grill-and-rotisserie-san-francisco",
+"loló-san-francisco-4",
+"20-spot-san-francisco",
+"stuffed-san-francisco",
+"beretta-san-francisco",
+"el-farolito-san-francisco-2",
+"californios-san-francisco",
+"kazan-san-francisco",
+"the-tradesman-san-francisco-2",
+"prubechu-san-francisco",
+"als-place-san-francisco",
+"limón-rotisserie-san-francisco-3",
+"lazy-bear-san-francisco-5",
+"local-mission-eatery-san-francisco",
+"the-front-porch-san-francisco",
+"paprika-san-francisco",
+"hog-and-rocks-san-francisco"
+]
+
+results_embarcadero = [
+"coqueta-san-francisco",
+"hog-island-oyster-co-san-francisco",
+"roli-roti-gourmet-rotisserie-san-francisco",
+"il-cane-rosso-san-francisco",
+"primavera-san-francisco",
+"la-mar-cebichería-peruana-san-francisco-4",
+"waterbar-san-francisco",
+"palomino-san-francisco-3",
+"chaya-brasserie-san-francisco",
+"4505-meats-san-francisco-7",
+"frog-hollow-farm-market-and-cafe-san-francisco",
+"perrys-san-francisco-6",
+"delica-san-francisco",
+"boccalone-san-francisco-2",
+"the-slanted-door-san-francisco",
+"el-porteño-empanadas-san-francisco-4",
+"hillstone-san-francisco-4",
+"pier-23-cafe-san-francisco",
+"fog-city-san-francisco-2",
+"gotts-roadside-san-francisco-2"
+]
+
+results_soma.map {
   |result| Restaurant.create(yelp_id: result)
 }
+
+results_mission.map {
+  |result| Restaurant.create(yelp_id: result)
+
+results_embarcadero.map {
+  |result| Restaurant.create(yelp_id: result)
+}
+
+
+
