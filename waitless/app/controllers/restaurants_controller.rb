1531f17bda6969ce0c9433a52adc63262bdcc39a
Diana Lozano & Jeff Chang <pair+cutofmyjib.jchang2014@devbootcamp.com>
2015-07-18 17:37:25 -0700

    Add first 20 seeds, base loc SOMA

--- waitless/app/controllers/restaurants_controller.rb
+++ waitless/app/controllers/restaurants_controller.rb
@@ -1,32 +1,35 @@
 class RestaurantsController < ApplicationController
 	def index
-		parameters = { term: params[:search], limit:10}
-		params[:location].length > 0 ? @location_filter = params[:location] : @location_filter = "San Francisco"
-
+		parameters = { term: "restaurants", limit:20}
+		@location_filter = "SOMA"
+    iterator = 0
   	response = Yelp.client.search(@location_filter, parameters).as_json
-  	@results = []
-  	for i in 0..9
-  		@business = response['hash']['businesses'][i]
-  		@categories = []
-  		@business['categories'].each do |category|
-  			@categories.push(category[0])
-  		end
-  		
-  		@results.push(
-  		{
-	  	name: "#{@business['name']}",
-	  	location: @business['location']['display_address'],
-	  	image: "#{@business['image_url']}",
-	  	rating: @business['rating_img_url'],
-	  	categories: @categories.join(', '),
-	  	latitude: @business['location']['coordinate']['latitude'],
-	  	longitude: @business['location']['coordinate']['longitude']
-	  	})
-  	end
+  	# @results = []
+  	# for i in 0..9
+  	# 	@business = response['hash']['businesses'][i]
+  	# 	@categories = []
+  	# 	@business['categories'].each do |category|
+  	# 		@categories.push(category[0])
+  	# 	end
 
-  	render 'index', locals: {results: @results}
-  	#render json: array
+  	# 	@results.push(
+  	# 	{
+	  # 	name: "#{@business['name']}",
+	  # 	location: @business['location']['display_address'],
+	  # 	image: "#{@business['image_url']}",
+	  # 	rating: @business['rating_img_url'],
+	  # 	categories: @categories.join(', '),
+	  # 	latitude: @business['location']['coordinate']['latitude'],
+	  # 	longitude: @business['location']['coordinate']['longitude']
+	  # 	})
+  	# end
 
+  	# render 'index', locals: {results: @results}
+    p "*" * 50
+    while iterator < 20
+      p response['hash']['businesses'][iterator]['id']
+      iterator += 1
+    end
 	end
 
   def show
