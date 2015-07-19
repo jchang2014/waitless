3a9fd20959c52213d17c52fd229488e7a3ac0bde
Jeff Chang <jeffrey.chang2014@gmail.com>
2015-07-19 12:00:09 -0700

    Fix controller issues. Implement conditional if yelp result is empty

--- waitless/app/controllers/restaurants_controller.rb
+++ waitless/app/controllers/restaurants_controller.rb
@@ -1,36 +1,37 @@
 class RestaurantsController < ApplicationController
 	def index
-		parameters = { term: "restaurants", limit:20}
-		@location_filter = "Embarcadero"
-    iterator = 0
-  	response = Yelp.client.search(@location_filter, parameters).as_json
-  	@results = []
-  	for i in 0..9
-  		@business = response['hash']['businesses'][i]
-      @restaurant = Restaurant.where(yelp_id: @business['id']).first
-      if @restaurant == nil 
-        @id = 'new' 
-      else
-        @id = @restaurant.id
-      end
+		parameters = { term: params[:search], limit:20}
+		@location_filter = (params[:location].length > 0) ? params[:location] : "San Francisco"
+  	@response = Yelp.client.search(@location_filter, parameters).as_json
 
-  		@categories = []
-  		@business['categories'].each do |category|
-  			@categories.push(category[0])
-  		end
+    if @response['hash']['total'] != 0
+    	@results = []
+    	for i in 0..9
+    		@business = @response['hash']['businesses'][i]
+        @restaurant = Restaurant.where(yelp_id: @business['id']).first
+        @restaurant == nil ? @id = 'new' : @id = @restaurant.id
 
-  		@results.push(
-  		{
-	  	name: "#{@business['name']}",
-	  	location: @business['location']['display_address'],
-	  	image: "#{@business['image_url']}",
-	  	rating: @business['rating_img_url'],
-	  	categories: @categories.join(', '),
-	  	latitude: @business['location']['coordinate']['latitude'],
-	  	longitude: @business['location']['coordinate']['longitude'],
-      id: @id
-	  	})
-  	end
+        #Pull only the "pretty" category names
+    		@categories = []
+    		@business['categories'].each do |category|
+    			@categories.push(category[0])
+    		end
+
+    		@results.push(
+    		{
+  	  	name: "#{@business['name']}",
+  	  	location: @business['location']['display_address'],
+  	  	image: "#{@business['image_url']}",
+  	  	rating: @business['rating_img_url'],
+  	  	categories: @categories.join(', '),
+  	  	latitude: @business['location']['coordinate']['latitude'],
+  	  	longitude: @business['location']['coordinate']['longitude'],
+        id: @id
+  	  	})
+    	end
+    else
+      @results = nil
+    end
 
   	render 'index', locals: {results: @results}
 
