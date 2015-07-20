3a8e9179807fb12464210a5754b980d5e32f409f
Jennifer Jaochico <jenjao515@gmail.com>
2015-07-19 17:55:25 -0700

    add basic form, no functionality

--- waitless/app/assets/javascripts/restaurants.js
+++ waitless/app/assets/javascripts/restaurants.js
@@ -1,2 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+// $(document).ready(function(){
+//     $(".quick-waitlist-form").hide();
+//   $(".result-button").click(function(){
+//     $(".quick-waitlist-form").slideToggle();
+//   });
+// });
+
