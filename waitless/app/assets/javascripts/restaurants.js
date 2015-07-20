478499d75f72f91c16c9d182b4f5c6429d24d9c9
Jennifer Jaochico <jenjao515@gmail.com>
2015-07-20 10:45:12 -0700

    clean up some leftover css

--- waitless/app/assets/javascripts/restaurants.js
+++ waitless/app/assets/javascripts/restaurants.js
@@ -1,9 +1,4 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-// $(document).ready(function(){
-//     $(".quick-waitlist-form").hide();
-//   $(".result-button").click(function(){
-//     $(".quick-waitlist-form").slideToggle();
-//   });
-// });
+
 
