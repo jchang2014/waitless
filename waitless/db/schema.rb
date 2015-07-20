0b7118ff643ad2d00c4a778977be38f5a4b86a1a
Jeff Chang <jeffrey.chang2014@gmail.com>
2015-07-20 09:58:47 -0700

    Add migration to change column timer to wait time for restaurants table

--- waitless/db/schema.rb
+++ waitless/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150718215649) do
+ActiveRecord::Schema.define(version: 20150720165729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20150718215649) do
     t.string   "yelp_id"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
-    t.integer  "timer",      default: 0
+    t.integer  "wait_time",  default: 0
   end
 
   create_table "types", force: :cascade do |t|
@@ -49,10 +49,11 @@ ActiveRecord::Schema.define(version: 20150718215649) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password_hash"
-    t.string   "phone_number"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.boolean  "admin"
+    t.boolean  "proprietor"
   end
 
 end
