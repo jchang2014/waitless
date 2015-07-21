ab71b2082276bcbc933349cf74cebe70843d42ad
Patrick S. Betti <patreek3@gmail.com>
2015-07-20 19:30:15 -0700

    forgot what i did

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,7 +3,6 @@ class Reservation < ActiveRecord::Base
 	belongs_to   :user
 	belongs_to   :restaurant
 
-
   def update_times
     update_column(:table_ready_time, (self.created_at + 15.minutes))
     update_column(:notify_time, (self.table_ready_time - 5.minutes))
