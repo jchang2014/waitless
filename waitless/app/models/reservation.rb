93f27cf5564c85a88a4df2073149846074c76d82
Patrick S. Betti <patreek3@gmail.com>
2015-07-20 20:42:08 -0700

    added after save methods using update column

--- waitless/app/models/reservation.rb
+++ waitless/app/models/reservation.rb
@@ -1,16 +1,18 @@
 class Reservation < ActiveRecord::Base
-
-	belongs_to :user
-	belongs_to :restaurant
+  after_save     :update_times
+  # after_save
+	belongs_to      :user
+	belongs_to      :restaurant
 
   def update_times
-    self.table_ready_time = self.created_at + 15.minutes
-    self.notify_time = self.table_ready_time - 5.minutes
-    self.save
+    update_column(:table_ready_time, (self.created_at + 15.minutes))
+    update_column(:notify_time, (self.table_ready_time - 5.minutes))
+    update_column(:notified?, false)
+    update_column(:table_ready, false)
   end
 
   def notify_now?
-    get_diff <= 5
+    get_diff <= 5 && self.notified? == false
   end
 
   #run sidekiq if notify_now?
@@ -19,6 +21,6 @@ class Reservation < ActiveRecord::Base
 
   private
     def get_diff
-      self.notify_time - Time.now)/3600
+      (self.notify_time - Time.now)/3600
     end
 end
