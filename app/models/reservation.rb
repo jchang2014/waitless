23913d7525d5c1c15f793d8cc362babda890de4a
Patrick S. Betti <patreek3@gmail.com>
2015-07-21 17:22:27 -0700

    Resolve gemfile conflict

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,23 @@
 class Reservation < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :restaurant
+
+  def update_times
+    self.table_ready_time = self.created_at + 15.minutes
+    self.notify_time = self.table_ready_time - 5.minutes
+    self.save
+  end
+
+  def notify_now?
+    get_diff <= 5
+  end
+
+  #run sidekiq if notify_now?
+
+
+
+  private
+    def get_diff
+      self.notify_time - Time.now)/3600
+    end
 end
