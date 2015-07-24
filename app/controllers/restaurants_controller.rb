43798c52f291df59cf432f4e9df39068ed4b2435
Jeff Chang <jeffrey.chang2014@gmail.com>
2015-07-23 22:42:26 -0700

    Add arbitrary wait time counter

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -25,7 +25,7 @@ class RestaurantsController < ApplicationController
           @restaurant = Restaurant.create(
             yelp_id: @business['id'],
             title: @business['name'],
-            wait_time: 30
+            wait_time: rand(31)
             )
         end
 
